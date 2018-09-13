11144d7c498d6fec19a6665cc91de8f263d895eb
Kevin Jung <rubic22@gmail.com>
2018-09-12 18:04:13 -0700

    now works on only attack page

--- closehosptravel.js
+++ closehosptravel.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         AutoCloseHosp'd
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  automatically closes torn profile pages if target is in the hospital
 // @author       WizardRubic
-// @match        *.torn.com/profiles.php*
+// @match        *.torn.com/loader.php*
 // @grant        window.close
 
 // ==/UserScript==
@@ -13,15 +13,18 @@
     'use strict';
     var isAttackable = function(description) {
         // if hospital or travelling then return false as they're not attackable
-        if(description=="" || description.indexOf("Okay")!=-1) {
+        if(description=="" || description.indexOf("Start fight")!=-1) {
             return true;
         } else {
             return false;
         }
     };
-    var profileElement = (document.getElementsByClassName("content-wrapper m-left20 left summer")[0]);
+    var profileElement = document.body;
     var callback = function(mutationsList) {
-        var description = document.getElementsByClassName("main-desc")[0].innerHTML;
+        if(document.getElementsByClassName("dialog___1j2Dg").length == 0) {
+           return;
+        }
+        var description = document.getElementsByClassName("dialog___1j2Dg")[0].innerHTML;
         if(!isAttackable(description)) {
             window.close();
         }
